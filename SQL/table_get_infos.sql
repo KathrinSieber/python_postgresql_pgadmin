1a11dedbace73b4a7ce0e1852a9e71ed52fe1651
Kathrin Sieber <siebekat@students.zhaw.ch>
2024-09-28 15:50:03 +0000

    update

--- SQL/table_get_infos.sql
+++ SQL/table_get_infos.sql
@@ -6,3 +6,38 @@ SELECT
   column_default
 FROM information_schema.columns
 WHERE TABLE_NAME = 'customers';
+
+-- 1. What are the details of all customers whose country is 'Spain'?
+SELECT * FROM Customers WHERE Country = 'Spain';
+
+-- 2. What are the distinct cities of customers from Germany with a city containing the letter 'B'?
+SELECT city FROM Customers WHERE Country = 'Germany' AND City ILIKE '%B%';
+
+3. What are the number of orders placed by each customer? Sort the result by the number of orders in descending order.
+
+
+4. What are the customers who have placed more than 3 orders?
+
+5. What are the top 5 most expensive products? Round the price to 2 decimal places.
+
+6. What are the order details (ProductID, Quantity) for customers from France?
+SELECT o.OrderID, c.CustomerName, c.Country, od.ProductID, od.Quantity FROM Orders o JOIN Customers c ON o.CustomerID = c.CustomerID JOIN OrderDetails od ON o.OrderID = od.OrderID WHERE c.Country = 'France';
+
+7. Area there products without a category assigned?
+
+8. What are all orders and their employees?
+
+9. What is the average, minimum, and maximum price of products? Round the values to 2 decimal places.
+
+10. What are the products with prices between 10 and 50? Round the price to 2 decimal places and sort the result by price in descending order.
+
+11. What are the shippers and the total number of orders shipped by each shipper, including those with no orders?
+
+12. What are the employees who have processed > 5 orders? Sort the result by the number of orders in descending order.
+
+13. What is the total revenue for each product within each order, including the product name and ordered by order ID and total revenue in descending order?
+
+14. What are the customers, employees, and the total number of orders placed by each customer?
+
+15. What are the products with an average price higher than the overall average product price? 
+    Round the price to 2 decimal places and sort the result by price in descending order.
